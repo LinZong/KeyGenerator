e09ce46c0844d2460e214d0fcdf39e4e9e9060a1
LinZong <353388639@qq.com>
2021-06-23 22:48:28 +0800

    introduce kotlintest framework

---
+++ key-generation-service/src/test/kotlin/moe/nemesiss/keygenerator/service/GreetingTest.kt
@@ -0,0 +1,10 @@
+package moe.nemesiss.keygenerator.service
+
+import io.kotlintest.matchers.numerics.shouldBeExactly
+import io.kotlintest.specs.FunSpec
+
+class GreetingTest : FunSpec({
+    test("test framework run smoothly") {
+        1 + 1 shouldBeExactly 2
+    }
+})
